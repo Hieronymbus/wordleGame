dbcbe607008f421d845ae418e014ee48b51edefa
Hieronymbuse <birdsallalexander1990@gmail.comm>
2024-03-27 12:19:37 +1300

    added assets folder for cssjs

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,15 @@ const theWordList = ["ROUTE","CRASH","CLOWN","SHAKY","VEGAN"] ;
 let word = theWordList[Math.floor(Math.random()*theWordList.length)] ;
 let guessCounter = 1;
 let letterCounter = 0;
-let green = "rgb(212,237,218)" ;
+let green = "#00FF00" ;
 let red = "rgb(255,192,199)" ;
 let brown = "rgb(161,115,62)" ;
 
-let correctAnswerArr = word.split("");
+//let alphabet = document.querySelectorAll(".alphabet")
+//alphabet.forEach((letter) => letter.style.backgroundColor = green)
 
+let correctAnswerArr = word.split("");
+console.log(correctAnswerArr)
 function handleLetterClick(letter) {
     let collection = document.getElementsByClassName("row" + guessCounter)
 
@@ -17,16 +20,27 @@ function handleLetterClick(letter) {
     }  
 }
 
-function compareWordLetters(correctAnswerArr, collection) {
+function compareWordLetters(correctAnswerArr, collection, keyboard) {
 
     for (let i = 0; i < 5; i++){
+
+
         if (correctAnswerArr[i] == collection[i].innerHTML) {
             collection[i].style.backgroundColor = green ;
+            // Dont do this. Instead add a class called "success"
+            document.getElementById(collection[i].innerHTML).style.backgroundColor = green;
+            document.getElementById(collection[i].innerHTML).classList.add("success");
         } else if (correctAnswerArr.includes(collection[i].innerHTML) ) {
             collection[i].style.backgroundColor = brown ;
+
+            // Check for the prsence of a class "success" rather than bgColro
+             if (document.getElementById(collection[i]).classlist.includes("success")){
+                document.getElementById(collection[i].innerHTML).style.backgroundColor = brown;
+             } 
         }
         else {
-            collection[i].style.backgroundColor = red ;
+            collection[i].style.backgroundColor = red;
+            document.getElementById(collection[i].innerHTML).style.backgroundColor = red;
         }
     }
 }
