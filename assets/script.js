7ddded33e7b00414b3ec6f80d9d44891587e10a2
Hieronymbuse <birdsallalexander1990@gmail.comm>
2024-04-04 14:31:55 +1300

    final commit for project deadline

--- assets/script.js
+++ assets/script.js
@@ -6,84 +6,62 @@ let letterCounter = 0;
 let green = "rgb(69,159,59)" ;
 let black = "rgb(14,14,14)" ;
 let brown = "rgb(161,116,61)" ;
-console.log(WORDS)
-
-
-let correctAnswerArr = word.toUpperCase().split("")
-
-
-console.log(correctAnswerArr)
+let correctAnswerArr = word.toUpperCase().split("");
 
 function handleLetterClick(event) {
     let letter = event.target.id;
 
-    let collection = document.getElementsByClassName("row" + guessCounter)
+    let collection = document.getElementsByClassName("row" + guessCounter);
 
     if (letterCounter >= 0 && letterCounter < 5) {
         collection[letterCounter].innerHTML = letter;
         letterCounter++;
-    }  
-}
+    };
+};
 
 function compareWordLetters(correctAnswerArr, collection, keyboard) {
 
     for (let i = 0; i < 5; i++){
-
-
         if (correctAnswerArr[i] == collection[i].innerHTML) {
             collection[i].style.backgroundColor = green ;
-            // Dont do this. Instead add a class called "success"
+            
             document.getElementById(collection[i].innerHTML).style.color = green;
             document.getElementById(collection[i].innerHTML).classList.add("success");
         } else if (correctAnswerArr.includes(collection[i].innerHTML) ) {
             collection[i].style.backgroundColor = brown ;
 
-            //check for class
             if (!document.getElementById(collection[i].innerHTML).classList.contains("success")){
                 document.getElementById(collection[i].innerHTML).style.color = brown;
-            } 
+            } ;
         }
         else {
             collection[i].style.backgroundColor = black;
             document.getElementById(collection[i].innerHTML).style.color = black;
-        }
-    }
+        };
+    };
     let guessWord = "";
-
-    
     for (let i = 0; i < 5; i++) {
         guessWord += collection[i].innerHTML;
-    }
+    };
     if (guessWord == word.toUpperCase() ){
         document.getElementById("alertBox").style.display = "flex"
-        document.getElementById("alertBox").innerHTML = "CORRECT WORD"
-    } 
-}
+        document.getElementById("alertBox").innerHTML = "CORRECT WORD, YOU WIN"
+    };
+};
 
 function handleCheck() {
     let collection = document.getElementsByClassName("row" + guessCounter);
-    
-    console.log(collection)
-    console.log(correctAnswerArr)
-    console.log(guessCounter)
-    console.log(letterCounter)
-    console.log(word)
-
     let guessWord = "";
-
-    
     for (let i = 0; i < 5; i++) {
         guessWord += collection[i].innerHTML;
-    }
-    
-
+    };
     if (letterCounter == 5 && WORDS.includes(guessWord.toLowerCase())) {
         
         compareWordLetters(correctAnswerArr, collection);
         
         letterCounter = 0;
         guessCounter++;
-    } else if(letterCounter == 5 && !WORDS.includes(guessWord.toUpperCase())){
+    } else if (letterCounter == 5 && !WORDS.includes(guessWord.toUpperCase())){
         document.getElementById("alertBox").style.display = "flex"
         document.getElementById("alertBox").innerHTML = "Not in Word List"
         setTimeout(function () {
@@ -95,14 +73,8 @@ function handleCheck() {
         setTimeout(function () {
             document.getElementById("alertBox").style.display = "none"
         }, 1100);
-    }
-
-    // if (guessWord == word.toUpperCase() ){
-    //     document.getElementById("alertBox").style.display = "flex"
-    //     document.getElementById("alertBox").innerHTML = "CORRECT WORD, YOU WIN THE GAME"
-    // } 
-    
-}
+    };  
+};
 
 function handleBackspace() {
     
@@ -123,9 +95,9 @@ function handleBackspace() {
     } else if (letterCounter == 1) {
         collection[0].innerHTML = ""
         letterCounter--;
-    }
+    };
     
-}
+};
 
 document.querySelectorAll(".alphabet").forEach(item => {
     item.addEventListener("click", handleLetterClick)
@@ -135,14 +107,14 @@ document.getElementById("backSpaceButton").addEventListener("click",handleBacksp
 
 document.getElementById("burgerMenuPopUpButton").addEventListener("click", () => {
     document.getElementById("burgerMenuPopUp").style.display = "block"
-})
+});
 document.getElementById("burgerMenuPopUp").addEventListener("mouseleave",()=>{
     document.getElementById("burgerMenuPopUp").style.display = "none"
-})
+});
 
 document.getElementById("displayHelpButton").addEventListener("click",() => {
     document.getElementById("helpPopUp").style.display = "block"
-} )
+});
 document.getElementById("helpPopUp").addEventListener("mouseleave",()=>{
     document.getElementById("helpPopUp").style.display = "none"
-})
+});
